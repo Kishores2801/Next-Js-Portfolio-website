793c6fee8630edc60e1d90491745582efe6bcd10
Kishore <87652286+Kishores2801@users.noreply.github.com>
2025-04-10 08:43:16 +0530

    update on hero section basic Layout

--- portfolio/src/app/components/hero.tsx
+++ portfolio/src/app/components/hero.tsx
@@ -8,12 +8,12 @@ type Props = {}
 const Hero = (props: Props) => {
   return (
     <div className='h-screen w-screen p-4 flex'>
-        <div className='w-1/3 m-4 border-2'>
+        <div className='w-1/3 m-4'>
           <div className='h-[25%]'></div>
-          <div className='h-[50%] w-[90%] m-2 p-4 text-2xl rounded-4xl text-blue-200 border-1 border-dashed '>Hello </div>
+          <div className='h-[50%] w-[90%] m-2 p-10 rounded-4xl text-shadow-lg'><span className='text-2xl hover:[110%]'>👋</span> Hello! My name is <span className='text-bold text-blue-400 text-center'><br/> Kishore Shanmugam</span> </div>
         </div>
-        <div className='w-2/3 m-4 border-2'>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, ullam illo. Inventore vero libero debitis enim blanditiis rerum ipsa ipsam quisquam architecto illum reiciendis officia, culpa, fugiat itaque vel ducimus.
+        <div className='w-2/3 m-4'>
+          <Lottie animationData={animationData} className="h-full" loop={true}/>
         </div>
     </div>
   )
